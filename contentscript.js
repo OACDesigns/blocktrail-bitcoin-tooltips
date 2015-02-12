547bcd3c6222e6c63c081e678507f7671cfa281c
Oisin <oisin@blocktrail.com>
2015-02-12 18:29:37 +0100

    add reveal for modal popup

--- contentscript.js
+++ contentscript.js
@@ -19,7 +19,10 @@ $(document).ready(function(){
     // on deactivate tab clearInterval(rescan);
     //...
 
+    //append the modal window to the body
 
+    var html = '<div id="myModal" class="reveal-modal"><h1>Modal Title</h1><p>Any content could go in here.</p>'
+             + '<a class="close-reveal-modal">&#215;</a></div>'
 
     chrome.runtime.sendMessage({data: 'hello world!', from: "content"});
 });
