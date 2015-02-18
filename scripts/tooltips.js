db5109904408ce93842c173634c7d00e3a2f6926
Oisin <oisin@blocktrail.com>
2015-02-18 11:36:23 +0100

    delay scanning on certain sites that modify DOM post load

--- scripts/tooltips.js
+++ scripts/tooltips.js
@@ -19,13 +19,32 @@ chrome.runtime.onMessage.addListener(function(data, sender, sendResponse) {
     return true;
 });
 
+/**
+ * checks the domain against a list of sites known to change DOM content after pageload,
+ * requiring a delayed scan for correct tooltip data to be created
+ */
+function detectDelayScanning() {
+    var hostname = window.location.origin;
+    var siteList = [
+        'facebook.com',
+        'twitter.com'
+    ];
+
+    var regex = new RegExp(siteList.join(),"i");
+    return !!hostname.match(regex);
+}
 
 $(document).ready(function(){
     var bt = new BlocktrailBitTips();
     console.log('Blocktrail tooltips extension...ready!');
 
     //scan the page and setup tooltips for any bitcoin/testnet addresses
-    bt.scan();
+    //detect if delayed scanning is required based on a list of known troublesome sites
+    if (detectDelayScanning()) {
+        setTimeout(function() {bt.scan();}, 2000);
+    } else {
+        bt.scan();
+    }
 
     //on activate tab, set rescan interval
     //chrome.tabs.onActivated.addListener(function (activeInfo) {
@@ -39,4 +58,5 @@ $(document).ready(function(){
 
     // on deactivate tab clearInterval(rescan);
     //...
+
 });
